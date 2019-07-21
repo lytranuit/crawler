28bd2d92d44ccc904fce2cb054d55c9f80c4c1b1
lytranuit <lytranuit@gmail.com>
2019-07-22 00:02:57 +0700

    update send telegram

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var open = require("open");
 var jsonfile = require('jsonfile')
 var file = './data.json'
 var data = jsonfile.readFileSync(file);
+var request = require('request');
 console.log("Ung dung thong bao chap moi");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
 var c = new Crawler({
     maxConnections: 10,
     // This will be called for each crawled page
@@ -14,7 +20,8 @@ var c = new Crawler({
         console.log("Finish : " + result.uri);
     }
 });
-
+var chatID = -361161345;
+var Telegram = true;
 //crawler_ChapMoi_F1();
 //crawler_ChapMoi_F2();
 crawler_ChapMoi_Kingdom();
@@ -76,30 +83,38 @@ function crawler_ChapMoi_F1() {
     }, 5000)
 }
 function crawler_ChapMoi_Kingdom() {
+    console.log("Track Kingdom")
     if (typeof data.Kingdom == "undefined" || data.Kingdom == null) {
         var chap = 0;
     } else {
         var chap = data.Kingdom;
     }
-    var page = "https://truyenmoi.xyz";
+    var page = "https://beeng.net";
     var time = setInterval(function () {
         c.queue({
-            uri: page + "/doc-truyen-tranh-kingdom-vuong-gia-thien-ha-manga",
-            callback: function (error, result, $) {
-//                console.log(result);
-//                console.log($);
-                console.log($(".select-chapter option:last-child"));
-                var text = $(".chapter-list a").eq(0).text()
+            uri: page + "/truyen-tranh-online/kingdom-vuong-gia-thien-ha-11449",
+            callback: function (error, res, $) {
+                var $ = res.$;
+//                console.log($(".manga-info-main"));
+//                return false;
+                var text = $(".manga-chapter .u84ho3 a").eq(0).text();
                 var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".list-chapter td a").eq(0).attr("href");
+                var href = $(".manga-chapter .u84ho3 a").eq(0).attr("href");
                 if (href.indexOf("http://") != -1 || href.indexOf("https://") != -1) {
 
                 } else {
                     href = page + href;
                 }
                 if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
+                    if (Telegram) {
+                        var messsage = "Kingdom chap " + new_chap + ": " + href;
+                        console.log(messsage)
+                        sendTelegram(chatID, messsage);
+                    } else {
+                        console.log(text);
+                        open(href);
+                    }
+
                     chap = new_chap;
                     data.Kingdom = chap;
                     save_data();
@@ -172,6 +187,8 @@ function crawler_ChapMoi_Taydu() {
     }, 5000)
 }
 function crawler_ChapMoi_NTGH() {
+
+    console.log("Track Nguoi trong giang ho")
     if (typeof data.NTGH == "undefined" || data.NTGH == null) {
         var chap = 0;
     } else {
@@ -194,8 +211,14 @@ function crawler_ChapMoi_NTGH() {
                     href = page + href;
                 }
                 if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
+                    if (Telegram) {
+                        var messsage = "NTGH chap " + new_chap + ": " + href;
+                        console.log(messsage);
+                        sendTelegram(chatID, messsage);
+                    } else {
+                        console.log(text);
+                        open(href);
+                    }
                     chap = new_chap;
                     data.NTGH = chap;
                     save_data();
@@ -206,4 +229,18 @@ function crawler_ChapMoi_NTGH() {
 }
 function save_data() {
     jsonfile.writeFileSync(file, data, {spaces: 2});
+}
+function sendTelegram($chatID, $messaggio) {
+    var $token = "606461497:AAH68TUT1mB3adaIxlud48-r-7fi2vADkRU";
+    var $url = "https://api.telegram.org/bot" + $token + "/sendMessage?chat_id=" + $chatID;
+    $url = $url + "&text=" + encodeURI($messaggio);
+    console.log($url);
+    request($url, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+//    c.queue({
+//        uri: $url,
+//    });
 }
