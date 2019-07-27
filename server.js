92721682bbe0107d8f27c5096eb168c61d8cce9a
lytranuit <lytranuit@gmail.com>
2019-07-27 22:14:40 +0700

    OnePiece và OnePunchMan

--- server.js
+++ server.js
@@ -22,66 +22,10 @@ var c = new Crawler({
 });
 var chatID = -361161345;
 var Telegram = true;
-//crawler_ChapMoi_F1();
-//crawler_ChapMoi_F2();
 crawler_ChapMoi_Kingdom();
-//crawler_ChapMoi_toriko();
-//crawler_ChapMoi_Taydu();
 crawler_ChapMoi_NTGH();
-function crawler_ChapMoi_F2() {
-    if (typeof data.NTGHF2 == "undefined" || data.NTGHF2 == null) {
-        var chap = 0;
-    } else {
-        var chap = data.NTGHF2;
-    }
-    var time = setInterval(function () {
-        c.queue({
-            uri: "http://teamtruyen.com/nguoi-trong-giang-ho-f2/",
-            callback: function (error, result, $) {
-                var text = $(".content .row_chap:first-child .tenChapter").text();
-                var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".content .row_chap:first-child a").attr("href");
-                if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
-                    chap = new_chap;
-                    data.NTGHF2 = chap;
-                    save_data();
-                }
-            }
-        });
-    }, 5000)
-}
-function crawler_ChapMoi_F1() {
-    if (typeof data.NTGHF1 == "undefined" || data.NTGHF1 == null) {
-        var chap = 0;
-    } else {
-        var chap = data.NTGHF1;
-    }
-    var page = "http://comicvn.net";
-    var time = setInterval(function () {
-        c.queue({
-            uri: page + "/truyen-tranh/nguoi-trong-giang-ho/162",
-            callback: function (error, result, $) {
-                var text = $(".list-chapter td a").eq(0).text()
-                var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".list-chapter td a").eq(0).attr("href");
-                if (href.indexOf("http://") != -1 || href.indexOf("https://") != -1) {
-
-                } else {
-                    href = page + href;
-                }
-                if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
-                    chap = new_chap;
-                    data.NTGHF1 = chap;
-                    save_data();
-                }
-            }
-        });
-    }, 5000)
-}
+crawler_ChapMoi_OnePunchMan();
+crawler_ChapMoi_OnePiece();
 function crawler_ChapMoi_Kingdom() {
     console.log("Track Kingdom")
     if (typeof data.Kingdom == "undefined" || data.Kingdom == null) {
@@ -89,17 +33,17 @@ function crawler_ChapMoi_Kingdom() {
     } else {
         var chap = data.Kingdom;
     }
-    var page = "https://beeng.net";
+    var page = "https://truyenqq.com";
     var time = setInterval(function () {
         c.queue({
-            uri: page + "/truyen-tranh-online/kingdom-vuong-gia-thien-ha-11449",
+            uri: page + "/truyen-tranh/kingdom-vuong-gia-thien-ha-245.html",
             callback: function (error, res, $) {
                 var $ = res.$;
 //                console.log($(".manga-info-main"));
 //                return false;
-                var text = $(".manga-chapter .u84ho3 a").eq(0).text();
+                var text = $(".works-chapter-list .works-chapter-item a").eq(0).text();
                 var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".manga-chapter .u84ho3 a").eq(0).attr("href");
+                var href = $(".works-chapter-list .works-chapter-item a").eq(0).attr("href");
                 if (href.indexOf("http://") != -1 || href.indexOf("https://") != -1) {
 
                 } else {
@@ -123,63 +67,82 @@ function crawler_ChapMoi_Kingdom() {
         });
     }, 5000)
 }
-function crawler_ChapMoi_toriko() {
-    if (typeof data.toriko == "undefined" || data.toriko == null) {
+function crawler_ChapMoi_OnePunchMan() {
+    console.log("Track Kingdom")
+    if (typeof data.OnePunchMan == "undefined" || data.OnePunchMan == null) {
         var chap = 0;
     } else {
-        var chap = data.toriko;
+        var chap = data.OnePunchMan;
     }
-    var page = "http://comicvn.net";
+    var page = "https://truyenqq.com";
     var time = setInterval(function () {
         c.queue({
-            uri: page + "/truyen-tranh/toriko/18",
-            callback: function (error, result, $) {
-                var text = $(".list-chapter td a").eq(0).text()
+            uri: page + "/truyen-tranh/onepunch-man-244.html",
+            callback: function (error, res, $) {
+                var $ = res.$;
+//                console.log($(".manga-info-main"));
+//                return false;
+                var text = $(".works-chapter-list .works-chapter-item a").eq(0).text();
                 var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".list-chapter td a").eq(0).attr("href");
+                var href = $(".works-chapter-list .works-chapter-item a").eq(0).attr("href");
                 if (href.indexOf("http://") != -1 || href.indexOf("https://") != -1) {
 
                 } else {
                     href = page + href;
                 }
                 if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
+                    if (Telegram) {
+                        var messsage = "OnePunchMan chap " + new_chap + ": " + href;
+                        console.log(messsage)
+                        sendTelegram(chatID, messsage);
+                    } else {
+                        console.log(text);
+                        open(href);
+                    }
+
                     chap = new_chap;
-                    data.toriko = chap;
+                    data.OnePunchMan = chap;
                     save_data();
                 }
             }
         });
     }, 5000)
 }
-function crawler_ChapMoi_Taydu() {
-    if (typeof data.Taydu == "undefined" || data.Taydu == null) {
+function crawler_ChapMoi_OnePiece() {
+    console.log("Track Kingdom")
+    if (typeof data.OnePiece == "undefined" || data.OnePiece == null) {
         var chap = 0;
     } else {
-        var chap = data.Taydu;
+        var chap = data.OnePiece;
     }
-    var page = "http://comicvn.net";
+    var page = "https://truyenqq.com";
     var time = setInterval(function () {
         c.queue({
-            uri: page + "/truyen-tranh/tay-du/11951",
-            callback: function (error, res, done) {
+            uri: page + "/truyen-tranh/dao-hai-tac-128.html",
+            callback: function (error, res, $) {
                 var $ = res.$;
 //                console.log($(".manga-info-main"));
 //                return false;
-                var text = $(".manga-chapter .u84ho3 a").eq(0).text();
+                var text = $(".works-chapter-list .works-chapter-item a").eq(0).text();
                 var new_chap = parseFloat(text.match(/-*[0-9]+/));
-                var href = $(".manga-chapter .u84ho3 a").eq(0).attr("href");
+                var href = $(".works-chapter-list .works-chapter-item a").eq(0).attr("href");
                 if (href.indexOf("http://") != -1 || href.indexOf("https://") != -1) {
 
                 } else {
                     href = page + href;
                 }
                 if (new_chap > chap) {
-                    console.log(text);
-                    open(href);
+                    if (Telegram) {
+                        var messsage = "OnePiece chap " + new_chap + ": " + href;
+                        console.log(messsage)
+                        sendTelegram(chatID, messsage);
+                    } else {
+                        console.log(text);
+                        open(href);
+                    }
+
                     chap = new_chap;
-                    data.Taydu = chap;
+                    data.OnePiece = chap;
                     save_data();
                 }
             }
